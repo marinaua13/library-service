f3ac95caf2295d41da6c2504dffbe71f432883e4
Marina <marina.ua13@gmail.com>
2024-08-12 11:16:40 +0300

    hide secret key and create .env.sample

--- library_service/settings.py
+++ library_service/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!q5qo$c15m2e_yxg%5q=bjcq3xq(i)&s=nj1@d2nc+gh$^9%g="
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default_secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
